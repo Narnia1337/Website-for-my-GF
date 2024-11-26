07ff3cf5089e792fef4715bfad986c2f8613edc7
Narnia1337 <devesh.bulani@gmail.com>
2024-11-25 22:51:12 -0700

    cry

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'tile.openstreetmap.org',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized: true,
   },
 }
 
 module.exports = nextConfig
-
