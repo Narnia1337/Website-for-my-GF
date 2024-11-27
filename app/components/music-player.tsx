f22821cf422f83e1e01540af40cfef53f108e27e
Narnia1337 <devesh.bulani@gmail.com>
2024-11-26 22:29:30 -0700

    pray pray pray pray

--- app/components/music-player.tsx
+++ app/components/music-player.tsx
@@ -1,13 +1,13 @@
 'use client'
 
 import { useEffect, useRef, useState } from 'react'
-import { Music, Pause, Play } from 'lucide-react'
+import { Pause, Play } from 'lucide-react'
 
 const song = {
   title: "Get You (feat. Kali Uchis)",
   artist: "Daniel Caesar",
-  src: "/get-you.mp3",  // Replace with the actual path to your MP3 file
-  startTime: 69  // Set this to the desired start time in seconds
+  src: "/path-to-your-audio-file.mp3",  // Replace with the actual path to your MP3 file
+  startTime: 0  // Set this to the desired start time in seconds
 }
 
 export function MusicPlayer() {
